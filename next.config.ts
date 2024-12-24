d2ebd4064aa543c3f10b29fde6e1c876b0fb122c
marangaa <rchdmaranga@gmail.com>
2024-12-24 16:32:35 +0300

    Add Cloudflare integration and update build scripts for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
 
 const nextConfig: NextConfig = {
   images: {
@@ -26,5 +28,9 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
